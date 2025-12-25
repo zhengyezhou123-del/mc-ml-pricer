203bd792591a146d72cb95b087f1132acc8aae40
zhengyezhou123-del <zhengyezhou123@gmail.com>
2025-12-24 16:45:14 -0800

    Update payoffs.py
    Add European option payoffs

--- mc_pricer/payoffs.py
+++ mc_pricer/payoffs.py
@@ -1,1 +1,17 @@
+import numpy as np
+
+def european_call(paths: np.ndarray, K: float) -> np.ndarray:
+    """
+    European call payoff: max(S_T - K, 0)
+    """
+    S_T = paths[:, -1]
+    return np.maximum(S_T - K, 0.0)
+
+
+def european_put(paths: np.ndarray, K: float) -> np.ndarray:
+    """
+    European put payoff: max(K - S_T, 0)
+    """
+    S_T = paths[:, -1]
+    return np.maximum(K - S_T, 0.0)
 
